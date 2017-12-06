04d58cf9ccfea34c7254be53a453c15e2a6011f2
Marcus Olsson <hello@marcusolsson.me>
2017-12-06 08:38:12 +0100

    Day 6 part 2

--- day06/solution.php
+++ day06/solution.php
@@ -4,7 +4,7 @@
      * @param  string  $puzzle
      * @return int
      */
-    function solve(string $puzzle) : int
+    function solve(string $puzzle, bool $debug = false) : int
     {
         $cycles = 0;
         $blocks = explode("\t", $puzzle);
@@ -36,18 +36,38 @@
 
             $cycles++;
 
-            $unique = in_array($blocks, $combinations);
-
-            $combinations[] = $blocks;
+            // Use hashes to more easily find duplicate keys
+            $unique = in_array(hashArray($blocks), $combinations);
+            $combinations[] = hashArray($blocks);
         }
 
         file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'output.txt', print_r($combinations, true));
 
+        if ($debug) {
+            $unique = array_unique($combinations);
+            $duplicates = array_diff_assoc($combinations, $unique);
+            $result = array_diff($unique, $duplicates);
+            $uniqueKeys = array_keys($result);
+            $duplicateKeys = array_keys(array_intersect($combinations, $duplicates));
+            return $duplicateKeys[1] - $duplicateKeys[0];
+        }
+
         return $cycles;
+    }
+
+    /**
+     * Serielize and md5-hash arrays
+     * @param  array  $value
+     * @return string        
+     */
+    function hashArray(array $value) : string
+    {
+        return md5(serialize($value));
     }
 
     // Use rtrim to remove new line, else we get an additional element
     $input = rtrim(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'input.txt'));
 
-    echo sprintf("Solution Part 1: %s \n", solve($input)); // 387096
+    echo sprintf("Solution Part 1: %s \n", solve($input)); // 11137
+    echo sprintf("Solution Part 2: %s \n", solve($input, true)); // 1037
     exit();
